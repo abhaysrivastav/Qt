b77567578db4cf3996a572b63ad996e70dc47bda
abhaysrivastav <abhaysrivastav@users.noreply.github.com>
2020-10-01 17:12:33 +0530

    QFuture Example - Getting the values

---
+++ qtca-1-15/main.cpp
@@ -0,0 +1,35 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QFuture>
+#include <QFutureWatcher>
+#include <QList>
+#include <QtConcurrent>
+
+int multiply(const int &value)
+{
+    qInfo() << "Called : " << value;
+    int num = value* 10;
+    return num;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    //Create a list and insert the 100 values in it
+    QList<int> list;
+    for (int i =0; i < 100; i++)
+    {
+        list.append (i);
+    }
+
+    QFutureWatcher<int> watcher;
+    QFuture<int> future = QtConcurrent::mapped(list, &multiply);
+    watcher.setFuture (future);
+    qInfo() << "Back in  Main !!! " ;
+    watcher.waitForFinished ();
+    qInfo() << "Done !";
+    qInfo() << future.results ();
+
+    return a.exec();
+}
